13eb18a35b20efd5c88a9ade1da32363846f9ddb
Liorshay054563 <liorshay054563@gmail.com>
2024-10-31 11:01:10 +0200

    Add files via upload

---
+++ hw13/main.py
@@ -0,0 +1,10 @@
+#start
+
+import my_func
+
+help(my_func)
+
+from my_func import print_stars
+for _ in range(5):
+    print_stars()
+
---
+++ hw13/my_func.py
@@ -0,0 +1,8 @@
+
+def print_stars():
+    """print the stars"""
+    print(f"******")
+
+
+if __name__ == "__main__":
+    print(__name__)
